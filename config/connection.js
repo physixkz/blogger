75b11e92fb866b9b20d691db704d0008d7515c4b
physiX <guerra.freddy@outlook.com>
2023-10-10 23:46:59 -0700

    removed added code for postresql

--- config/connection.js
+++ config/connection.js
@@ -3,25 +3,19 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.DATABASE_URL) {
-    sequelize = new Sequelize(process.env.DATABASE_URL, {
-      dialect: 'postgres',
-      protocol: 'postgres',
-      logging: true,
-      ssl: true,
-    });
-  } else {
-    sequelize = new Sequelize(
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
       process.env.DB_NAME,
       process.env.DB_USER,
-      process.env.DB_PASSWORD,
+      process.env.DB_PASSWORD, 
       {
-        host: 'ec2-54-234-13-16.compute-1.amazonaws.com',
-        dialect: 'postgres',
-        port: 5432,
-        password: 'd64d8ec10b015f043a745dc79974d682f57453dce9740ee6e1398edcffc8bc6c',
+          host: 'localhost',
+          dialect: 'mysql',
+          port: 3306
       }
-    );
-  }
-  
-  module.exports = sequelize;
+  );
+}
+
+module.exports = sequelize;
