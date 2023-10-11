f04f830fbcd420550d17ecb6f284d5e2ea9f5bf0
physiX <guerra.freddy@outlook.com>
2023-10-10 22:54:47 -0700

    changing connection for heroku testing

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,20 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
-
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'mysql',
+  });
 } else {
+  // Your local configuration
   sequelize = new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD, 
-      {
-          host: 'localhost',
-          dialect: 'mysql',
-          port: 3306
-      }
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    }
   );
 }
 
