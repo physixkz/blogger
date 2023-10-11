a7735ab77c63fd5691b44225c99a8acd0b62b3bd
physiX <guerra.freddy@outlook.com>
2023-10-10 23:20:45 -0700

    heroku testing

--- config/connection.js
+++ config/connection.js
@@ -4,28 +4,23 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.DATABASE_URL) {
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres', // Use 'postgres' for PostgreSQL
-    protocol: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // You may need to set this to true based on your PostgreSQL setup
-      },
-    },
-  });
-} else {
-  // Your local configuration
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306,
-    }
-  );
-}
-
-module.exports = sequelize;
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      protocol: 'postgres',
+      logging: true,
+    });
+  } else {
+    sequelize = new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: 'ec2-54-234-13-16.compute-1.amazonaws.com',
+        dialect: 'postgres',
+        port: 5432,
+        password: 'd64d8ec10b015f043a745dc79974d682f57453dce9740ee6e1398edcffc8bc6c',
+      }
+    );
+  }
+  
+  module.exports = sequelize;
